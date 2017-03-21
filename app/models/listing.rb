46f7b40c753e96cdbbaa7ada57da26fde42114f7
Chris Nowell <cnowell23@gmail.com>
2017-03-21 00:27:24 +0000

    add list to user

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,10 @@ class Listing < ActiveRecord::Base
 validates :name, :description, :price, presence: true
 validates :price, numericality: {greater_than: 0}
 validates_attachment_presence :image
+
+
+belongs_to :user
+
 end
 
 
